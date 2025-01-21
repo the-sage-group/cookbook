ee1684e08ec525b492e080c86aaf929d1b240c46
trumanpurnell <truman.purnell@gmail.com>
2025-01-20 20:49:22 -0700

    really shitty wiring of handlers to incoming events

--- index.ts
+++ index.ts
@@ -1,14 +1,23 @@
-import { AwyesClient, credentials, Node } from "@the-sage-group/awyes-node";
+import {
+  AwyesClient,
+  credentials,
+  Node,
+  Event,
+  EventType,
+} from "@the-sage-group/awyes-node";
 
 import * as aws from "./aws";
+import { clients } from "./aws/clients";
 
 const client = new AwyesClient("localhost:50051", credentials.createInsecure());
+const router = {};
 
 for (const { node, handler } of aws.nodes) {
   client.registerNode({ node: node as Node }, (error, response) => {
     if (error) {
-      console.error(error);
+      throw error;
     } else {
+      router[`${node.context}.${node.name}`] = handler;
       console.dir(response, { depth: null });
     }
   });
@@ -16,9 +25,29 @@ for (const { node, handler } of aws.nodes) {
 
 const stream = client.runAndWait();
 
-stream.on("data", (response) => {
+stream.on("data", async (response: Event) => {
   // Handle each response from the server
   console.log("Received response:", response);
+  switch (response.type) {
+    case EventType.EXECUTING:
+      const { node } = response;
+      if (!node) {
+        throw new Error("Node is undefined");
+      }
+      const handler = router[`${node.context}.${node.name}`];
+      const result = await handler(clients, node.parameters);
+      stream.write({
+        type: EventType.COMPLETED,
+        node: node,
+        trip: response.trip,
+        label: result.label,
+        timestamp: Date.now(),
+      });
+      break;
+    default:
+      console.log("Unknown event type:", response.type);
+      break;
+  }
 });
 
 stream.on("error", (error) => {
